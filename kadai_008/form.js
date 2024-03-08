b6698a0012c9bb6a5203d3d56d944393b3aa6278
ami620 <rrte_620@outlook.jp>
2024-03-08 15:58:36 +0900

    5thcommit

---
+++ kadai_008/form.js
@@ -0,0 +1,9 @@
+// $('btn').on("click", function () {
+//   const p = $('p').text();
+//   console.log(p);
+//   $('p').text('jQueryを勉強しよう');
+// });
+
+$('.btn').on("click", function () {
+  $(.text-box).val(クリックしました);
+});
---
+++ kadai_010/summary.js
@@ -0,0 +1,28 @@
+// 1	「文字色変化」ボタンを押した際に、文字色が任意の色に更新されている
+// 2	「文字内容変化」ボタンを押した際に、任意の文字内容に更新されている
+// 3	「フェードアウト」ボタンを押した際に、フェードアウトで文字が消える
+// 4	「フェードイン」ボタンを押した際に、フェードインで文字が現れる
+
+$(function() {
+  $('#change-color').on('click', function(){
+    $('#target').css('color', 'red');
+  });
+});
+
+$(function() {
+  $('#change-text').on('click', function(){
+    $('#target').text('キーが押されました！');
+  });
+});
+
+$(function() {
+  $('fade-out').on('click', function(){
+    $('#target').fadeOut();
+  });
+});
+
+$(function() {
+  $('#fade-in').on('click', function(){
+    $('#target').fadeIn();
+  });
+});
