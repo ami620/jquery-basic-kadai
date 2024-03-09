deb73d6691fc2d270fa072945208b4f2de7bc56d
ami620 <rrte_620@outlook.jp>
2024-03-09 10:49:11 +0900

    7th commit

--- kadai_008/form.js
+++ kadai_008/form.js
@@ -1,9 +1,5 @@
-// $('btn').on("click", function () {
-//   const p = $('p').text();
-//   console.log(p);
-//   $('p').text('jQueryを勉強しよう');
-// });
-
-$('.btn').on("click", function () {
-  $(.text-box).val(クリックしました);
+$(function () {
+  $('.btn').on('click', function () {
+    $('.text-box').val ('クリックしました！');
+  });  
 });
